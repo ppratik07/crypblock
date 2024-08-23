ca5553ab7214f470fd4b8a9b2237a5985db9ed5a
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-08-24 01:40:26 +0530

    Landing page

--- cryblock/src/components/LandingPage.tsx
+++ cryblock/src/components/LandingPage.tsx
@@ -10,18 +10,28 @@ export const LandingPage = () => {
             <div className="pt-40 mr-96 text-xl text-gray-400 font-semibold">
                 Choose how you would like to configure you wallet :
             </div>
-            <div className="pt-10 ml-20">
-                <div className="max-w-56 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-200 dark:border-gray-200">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 mb-8">
+            <div className="pt-10 ml-48">
+                <div className="max-w-56 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-200 dark:border-gray-200 relative overflow-hidden transition-colors duration-300 hover:bg-green-200">
+                    <div className="absolute inset-0 bg-green-200 opacity-0 transition-opacity duration-300 hover:opacity-100 z-10"></div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 mb-8 relative z-20">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                     </svg>
                     <a href="#">
-                        <h5 className="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-black">Create New Wallet</h5>
+                        <h5 className="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-black relative z-20">Create New Wallet</h5>
                     </a>
                 </div>
+                <div className="flex justify-center -mt-36 mr-72">
+                <div className="max-w-56 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-200 dark:border-gray-200 relative overflow-hidden transition-colors duration-300 hover:bg-green-200">
+                    <div className="absolute inset-0 bg-blue-400 opacity-0 transition-opacity duration-300 hover:opacity-100 z-10"></div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 mb-8 relative z-20">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15M9 12l3 3m0 0 3-3m-3 3V2.25" />
+                        </svg>
+                        <a href="#">
+                        <h5 className="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-black relative z-20">Import Wallet</h5>
+                    </a>
+                    </div>
+                </div>
             </div>
-
-
         </div>
 
     )
