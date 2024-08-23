431df804b874e68f1faec59470c50b7687d6b645
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-08-24 02:03:03 +0530

    added a mneumonic

--- cryblock/src/App.tsx
+++ cryblock/src/App.tsx
@@ -1,12 +1,22 @@
+import { generateMnemonic } from 'bip39';
 import './App.css'
+import { useState } from 'react';
+import { LandingPage } from './components/LandingPage';
 
 function App() {
-
+  const [mnemonic, setMnemonic] = useState("");
 
   return (
-    <>
-    
-    </>
+    <div>
+      <input type="text" value={mnemonic} ></input>
+      <button onClick={async function () {
+        const mn = await generateMnemonic();
+        setMnemonic(mn);
+      }}>
+        Create Seed Phrase
+      </button>
+      <LandingPage/>
+    </div>
   )
 }
 
