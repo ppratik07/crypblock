4003fb320414402e56eed1ed2a121a95f92ef49b
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-08-24 14:18:27 +0530

    Updating and routing pages in app.tsx

--- cryblock/src/App.tsx
+++ cryblock/src/App.tsx
@@ -2,20 +2,27 @@ import { generateMnemonic } from 'bip39';
 import './App.css'
 import { useState } from 'react';
 import { LandingPage } from './components/LandingPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { PasswordComponent } from './components/PasswordComponent';
 
 function App() {
   const [mnemonic, setMnemonic] = useState("");
 
   return (
     <div>
-      <input type="text" value={mnemonic} ></input>
+      {/* <input type="text" value={mnemonic} ></input>
       <button onClick={async function () {
         const mn = await generateMnemonic();
         setMnemonic(mn);
       }}>
         Create Seed Phrase
-      </button>
-      <LandingPage/>
+      </button> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/password" element={<PasswordComponent />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
