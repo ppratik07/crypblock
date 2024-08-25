45fc73e3151a20c4085cec097625d8a8ba24dbf6
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-08-25 22:17:29 +0530

    Password component

--- cryblock/src/page/PasswordComponent.tsx
+++ cryblock/src/page/PasswordComponent.tsx
@@ -1,11 +1,26 @@
-import { BackIcon } from "../components/BackIcon"
+import React, { useState } from 'react';
+import { BackIcon } from "../components/BackIcon";
+import { useNavigate } from 'react-router-dom';
 
 export const PasswordComponent = () => {
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const navigate = useNavigate();
+    const handleSubmit = () => {
+        if (password !== confirmPassword) {
+            setErrorMessage("Passwords do not match");
+        } else {
+            setErrorMessage('');
+            console.log("Passwords match");
+            navigate("/recoveryphase");
+        }
+    };
+
     return (
         <div className="mt-20 flex">
-
             <div className="flex-1 ml-5 flex flex-col items-center">
-              <BackIcon/>
+                <BackIcon />
                 <div className="text-center">
                     <div className="text-4xl font-bold mb-2">
                         Choose a Password
@@ -20,6 +35,8 @@ export const PasswordComponent = () => {
                         <input
                             type="password"
                             placeholder="Choose Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                             className="peer h-full w-full rounded-[7px] border border-gray-300 bg-transparent bg-white px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 shadow-lg shadow-gray-900/5 placeholder:text-gray-500 focus:border-2 focus:border-gray-900 focus:outline-0 focus:ring-gray-900/10"
                         />
                     </div>
@@ -27,18 +44,26 @@ export const PasswordComponent = () => {
                         <input
                             type="password"
                             placeholder="Re-enter Password"
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
                             className="peer h-full w-full rounded-[7px] border border-gray-300 bg-transparent bg-white px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 shadow-lg shadow-gray-900/5 placeholder:text-gray-500 focus:border-2 focus:border-gray-900 focus:outline-0 focus:ring-gray-900/10"
                         />
                     </div>
-                    <button className="bg-gray-500 hover:bg-blue-700 text-white font-bold py-2 px-40 rounded-full">
+                    {errorMessage && (
+                        <div className="text-red-500 text-sm">
+                            {errorMessage}
+                        </div>
+                    )}
+                    <button 
+                        className="bg-gray-500 hover:bg-blue-700 text-white font-bold py-2 px-40 rounded-full"
+                        onClick={handleSubmit}
+                    >
                         Submit
                     </button>
                 </div>
             </div>
 
-
             <div className="w-px bg-gray-300 mx-8"></div>
-
 
             <div className="flex-1 px-5">
                 <div className='mt-10'>
@@ -48,7 +73,7 @@ export const PasswordComponent = () => {
                 </div>
                 <div className="text-lg text-gray-700 mt-8">
                     <p>
-                        For your protection,the password is stored securely on your device. We will not be able to recover it for you, so make sure you remember it.
+                        For your protection, the password is stored securely on your device. We will not be able to recover it for you, so make sure you remember it.
                     </p>
                 </div>
             </div>
