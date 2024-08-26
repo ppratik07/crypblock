ca732b8e2e63754da58918024cfe0743bca019f2
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-08-27 00:48:31 +0530

    App.tsx commit

--- cryblock/src/App.tsx
+++ cryblock/src/App.tsx
@@ -1,4 +1,3 @@
-import { generateMnemonic } from 'bip39';
 import './App.css'
 import { LandingPage } from './page/LandingPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -7,21 +6,24 @@ import { RecoveryPhase } from './page/RecoveryPhase';
 import { ReadyToUse } from './page/ReadyToUse';
 import WalletView from './page/SolanaWallet';
 import EtheriumWallet from './page/EtheriumWallet';
+import WalletConnectionProvider from './components/WalletConnectionProvider';
 
 function App() {
 
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/password" element={<PasswordComponent />} />
-          <Route path="/recoveryphase" element={<RecoveryPhase />} />
-          <Route path="/ready" element={<ReadyToUse />} />
-          <Route path="/solanawallet" element={<WalletView />} />
-          <Route path="/eth" element={<EtheriumWallet />} />
-        </Routes>
-      </BrowserRouter>
+      <WalletConnectionProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/password" element={<PasswordComponent />} />
+            <Route path="/recoveryphase" element={<RecoveryPhase />} />
+            <Route path="/ready" element={<ReadyToUse />} />
+            <Route path="/solanawallet" element={<WalletView />} />
+            <Route path="/eth" element={<EtheriumWallet />} />
+          </Routes>
+        </BrowserRouter>
+      </WalletConnectionProvider>
     </div>
   )
 }
