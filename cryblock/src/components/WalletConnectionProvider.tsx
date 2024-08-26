0a8a9c8177c1ceb5ed5e71884af61b0a930daf4a
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-08-27 00:48:45 +0530

    Connect you wallet component

---
+++ cryblock/src/components/WalletConnectionProvider.tsx
@@ -0,0 +1,31 @@
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import React, { useMemo } from "react";
+import "@solana/wallet-adapter-react-ui/styles.css";
+
+const WalletConnectionProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(() => [
+    new PhantomWalletAdapter()
+  ], [network]);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          {children}
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default WalletConnectionProvider;
